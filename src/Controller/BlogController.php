9844e78c8131e153ab47c7f792c139101acda98d
Pierre Maurand Ovassa <pierremaurand@gmail.com>
2022-09-15 09:16:55 +0100

    Update base and index, create home and show

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('blog/home.html.twig');
+    }
+
+    #[Route('/blog/12', name: 'app_show')]
+    public function show(): Response
+    {
+        return $this->render('blog/show.html.twig');
+    }
+}
